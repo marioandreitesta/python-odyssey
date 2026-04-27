f68fd05a91acf1e968593cf77d88c18735a2ee91
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-26 21:06:11 -0300

    chore:(exercicio#034): adiciona melhor organização

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#034.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#034.py
@@ -3,6 +3,7 @@
 # Para os inferiores ou iguais, o aumento é de 15%
 
 def receber_salario():
+    
     while True:
         try:
             salario_usuario = float(input("Digite o valor de seu salário [R$]: "))
@@ -11,9 +12,11 @@ def receber_salario():
             print("Erro: é aceito apenas números, tente novamente!")
 
 def calcular_aumento(salario):
+
     if salario > 1250:
         salario *= 1.10
         print(f"Seu salário recebeu um aumento de 10%: R${salario:.2f}")
+        
     else:
         salario *= 1.15
         print(f"Seu salário recebeu um aumento de 15%: R${salario:.2f}")

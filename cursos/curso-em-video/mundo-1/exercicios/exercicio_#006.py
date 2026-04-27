5c6cc26d4c5d6cd6bde6b8d0e996d09d4134c6d9
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-27 14:18:47 -0300

    refactor(exercicio_#006): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#006.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#006.py
@@ -1,14 +1,16 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
-print("Digite um número para ver seu dobro, triplo e raiz quadrada.")
+from cores import VERDE_BOLD, VERMELHO_BOLD, AZUL_BOLD, LIMPAR, CINZA_BOLD
 
-numero = int(input("Digite o número: "))
+print(f"Digite um número para ver seu {VERMELHO_BOLD}dobro, {VERDE_BOLD}triplo{LIMPAR} e {AZUL_BOLD}raiz quadrada.{LIMPAR}")
+
+numero = int(input(f"{CINZA_BOLD}Digite o número: {LIMPAR}"))
 
 dobro = numero * 2 
 triplo = numero * 3
 raiz_quadrada = numero ** 0.5
 
-print("Analisamos o número informado:")
-print(f"Dobro: {dobro}")
-print(f"Triplo: {triplo}")
-print(f"Raiz quadrada: {raiz_quadrada:.0f}")
+print(f"{CINZA_BOLD}Analisamos o número informado:{LIMPAR}")
+print(f"Dobro: {VERMELHO_BOLD}{dobro}{LIMPAR}")
+print(f"Triplo: {VERDE_BOLD}{triplo}{LIMPAR}")
+print(f"Raiz quadrada: {AZUL_BOLD}{raiz_quadrada:.0f}{LIMPAR}")

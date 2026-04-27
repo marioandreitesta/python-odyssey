f25543f6aa650bfa2c186b6dbb5dfee7756a8426
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-27 11:24:39 -0300

    refactor(exercicio_#003): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#003.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#003.py
@@ -1,9 +1,9 @@
 # Crie um programa que leia dois números e mostre a soma entre eles.
 
-print("Escreva os números que deseja realizar a soma!")
+print("\033[1;30;44mEscreva os números que deseja realizar a soma!\033[m")
 
 primeiroNumero = int(input("Digite o primeiro número: "))
 segundoNumero = int(input("Digite o segundo número: "))
 resultado_soma = primeiroNumero + segundoNumero
 
-print(f"A soma entre os números escolhidos é: {resultado_soma}")
+print(f"\033[0;34mA soma entre os números escolhidos é:\033[m \033[1;36m{resultado_soma}\033[m")

4937fbed102aab95047ae402cbfa6c856cb4c965
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-27 22:26:40 -0300

    refactor(exercicio_#012): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#012.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#012.py
@@ -1,14 +1,17 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
-print("Calculador de Descontos!")
+from cores import CINZA_BOLD
+from cores import AMARELO_BOLD, AMARELO_BACKGROUND, LIMPAR, VERMELHO_BOLD, VERDE_BOLD
 
-preco_produto = float(input("Digite o preço do seu produto: R$"))
+print(f"{AMARELO_BACKGROUND}Calculador de Descontos!{LIMPAR}")
+
+preco_produto = float(input(f"{CINZA_BOLD}Digite o preço do seu produto: {AMARELO_BOLD}R$"))
 
 preco_final = preco_produto * 0.95
 desconto = preco_final - preco_produto
 
-print("-" * 40)
+print(f"{LIMPAR}{CINZA_BOLD}-{LIMPAR}" * 40)
 
-print(f"Preço do produto: R${preco_produto:.2f}")
-print(f"Desconto (5%): R${desconto:.2f}")
-print(f"Valor final: R${preco_final:.2f}")
+print(f"{CINZA_BOLD}Preço do produto: {AMARELO_BOLD}R${preco_produto:.2f}")
+print(f"{CINZA_BOLD}Desconto (5%): {VERMELHO_BOLD}R${desconto:.2f}")
+print(f"{CINZA_BOLD}Valor final: {VERDE_BOLD}R${preco_final:.2f}{LIMPAR}")

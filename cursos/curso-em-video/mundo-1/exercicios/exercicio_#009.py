90a6d64dd4cc637d6672f55fdc74722ce1322da1
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-27 21:41:09 -0300

    refactor(exercicio_#009): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#009.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#009.py
@@ -1,9 +1,13 @@
 # Crie um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
 
-print("Gerador de Tabuada")
-num = int(input("Digite um número para ver a sua tabuada: "))
-print("-" * 12)
-print(f"{num} x 1 = {num * 1}")
+from cores import LIMPAR, CINZA_BOLD, AMARELO_UNDERLINE
+
+
+print(f"{AMARELO_UNDERLINE}Gerador de Tabuada{LIMPAR}")
+num = int(input(f"{AMARELO_UNDERLINE}Digite um número para ver a sua tabuada: "))
+
+print(f"{LIMPAR}-" * 45)
+print(f"{CINZA_BOLD}{num} x 1 = {num * 1}")
 print(f"{num} x 2 = {num * 2}")
 print(f"{num} x 3 = {num * 3}")
 print(f"{num} x 4 = {num * 4}")
@@ -12,5 +16,5 @@ print(f"{num} x 6 = {num * 6}")
 print(f"{num} x 7 = {num * 7}")
 print(f"{num} x 8 = {num * 8}")
 print(f"{num} x 9 = {num * 9}")
-print(f"{num} x 10 = {num * 10}")
-print("-" * 12)
+print(f"{num} x 10 = {num * 10}{LIMPAR}")
+print("-" * 45)

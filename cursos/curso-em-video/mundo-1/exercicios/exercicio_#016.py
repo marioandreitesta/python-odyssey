2c8bb31aebdd0dd1e4f5df9ffeeb6702c9385afe
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 15:28:54 -0300

    refactor(exercicio_#016): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#016.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#016.py
@@ -3,14 +3,15 @@
 # O número 6.127 tem a parte inteira 6. 
 
 from math import trunc
+from cores import LIMPAR, AZUL_BOLD, AZUL_BACKGROUND, CINZA_BOLD, VERDE_UNDERLINE
 
-print("="*40)
-print(f"{'CONSULTE A PORÇÃO INTEIRA DE UM NÚMERO':^40}")
-print("="*40)
+print(f"{AZUL_BOLD}={LIMPAR}"*40)
+print(f"{AZUL_BACKGROUND}{'CONSULTE A PORÇÃO INTEIRA DE UM NÚMERO':^40}{LIMPAR}")
+print(f"{AZUL_BOLD}={LIMPAR}"*40)
 
-numero = float(input("Digite um número: "))
-print("-" * 40)
+numero = float(input(f"{CINZA_BOLD}Digite um número:{LIMPAR} {VERDE_UNDERLINE}"))
+print(f"{AZUL_BOLD}={LIMPAR}"*40)
 
 porcao_int = trunc(numero)
 
-print(f"Porção inteira do número é: {porcao_int}")
+print(f"{CINZA_BOLD}Porção inteira do número é: {VERDE_UNDERLINE}{porcao_int}{LIMPAR}")

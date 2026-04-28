593a9fa97fb0ff5f1770d7e5a619263e26c3d9bd
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 19:10:41 -0300

    refactor(exercicio_#026): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#026.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#026.py
@@ -1,12 +1,15 @@
 # Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez. 
+
+from cores import LIMPAR, CINZA_BOLD, CIANO_UNDERLINE, CIANO_BOLD
+
 DIV = 40
 
-frase = input('Digite uma frase: ').strip()
-print('' * DIV)
+frase = input(f'{CINZA_BOLD}Digite uma frase: {CIANO_BOLD}').strip()
+print(f'{LIMPAR}' * DIV)
 
 frase_formatada = frase.lower()
 contando_a = frase.count('a')
 
-print(f'Quantas vezes a palavra "A" aparece na frase: {contando_a}')
-print(f'Posição da primeira aparição: {frase_formatada.find('a')}')
-print(f'Posição da última aparição: {frase_formatada.rfind('a')}')
+print(f'{CINZA_BOLD}Quantas vezes a palavra {CIANO_BOLD}"A" {CINZA_BOLD}aparece na frase: {CIANO_UNDERLINE}{contando_a}{LIMPAR}')
+print(f'{CINZA_BOLD}Posição da primeira aparição: {CIANO_UNDERLINE}{frase_formatada.find('a')}')
+print(f'{LIMPAR}{CINZA_BOLD}Posição da última aparição: {CIANO_BOLD}{frase_formatada.rfind('a')}{LIMPAR}')

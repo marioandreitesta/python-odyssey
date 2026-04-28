dc742ce6c072be5807d7b2ce2b44b562d1a92290
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-27 22:16:45 -0300

    refactor(exercicio_#011): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#011.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#011.py
@@ -1,18 +1,20 @@
 # Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quatidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados. 
 
-print("Vamos começar a calcular a área de sua parede!")
+from cores import ROXO_BACKGROUND, LIMPAR, CINZA_BOLD, ROXO_BOLD
 
-altura_parede = float(input("Digite a altura de sua parede: "))
+print(f"{ROXO_BACKGROUND}Vamos começar a calcular a área de sua parede!{LIMPAR}")
+
+altura_parede = float(input(f"{CINZA_BOLD}Digite a altura de sua parede: "))
 largura_parede = float(input("Digite a largura de sua parede: "))
 
-print("Calculando área...")
+print(f"{LIMPAR}{ROXO_BOLD}Calculando área...{LIMPAR}")
 
 area_parede = largura_parede * altura_parede
 
-print(f"A área de sua parede é {area_parede:.1f}m²")
+print(f"{CINZA_BOLD}A área de sua parede é{LIMPAR} {ROXO_BOLD}{area_parede:.1f}m²{LIMPAR}")
 
-print("Agora vamos calcular o rendimento da tinta!")
+print(f"{ROXO_BACKGROUND}Agora vamos calcular o rendimento da tinta!{LIMPAR}")
 
 rendimento_tinta = area_parede / 2
 
-print(f"Vai ser necessário {rendimento_tinta:.1f} litros de tinta para cobrir toda a área.")
+print(f"{CINZA_BOLD}Vai ser necessário {ROXO_BOLD}{rendimento_tinta:.1f}{LIMPAR} {CINZA_BOLD}litros de tinta para cobrir toda a área.{LIMPAR}")

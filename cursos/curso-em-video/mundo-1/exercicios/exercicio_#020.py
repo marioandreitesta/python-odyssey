e09767bdf20ac15d93cd711a3d75b3535053cec3
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 17:00:48 -0300

    refactor(exercicio_#020): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#020.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#020.py
@@ -2,25 +2,26 @@
 # Faça um programa que leia o nome dos alunos e mostre a ordem sorteada.
 
 from random import shuffle
+from cores import AZUL_BOLD, LIMPAR, CINZA_BOLD, VERDE_BOLD, AMARELO_BOLD, VERMELHO_BOLD
 
 DIV = 45
 
-print("="*DIV)
+print(f"{AZUL_BOLD}={LIMPAR}"*DIV)
 print("LISTA DE APRESENTAÇÃO DOS ALUNOS".center(DIV))
-print("="*DIV)
+print(f"{AZUL_BOLD}={LIMPAR}"*DIV)
 
-aluno0 = input("Digite o nome do 1° aluno: ")
-aluno1 = input("Digite o nome do 2° aluno: ")
-aluno2 = input("Digite o nome do 3° aluno: ")
-aluno3 = input("Digite o nome do 4° aluno: ")
-print("-"*DIV)
+aluno0 = input(f"{CINZA_BOLD}Digite o nome do {VERDE_BOLD}1° {CINZA_BOLD}aluno: {LIMPAR}")
+aluno1 = input(f"{CINZA_BOLD}Digite o nome do {AZUL_BOLD}2° {CINZA_BOLD}aluno: {LIMPAR}")
+aluno2 = input(f"{CINZA_BOLD}Digite o nome do {AMARELO_BOLD}3° {CINZA_BOLD}aluno: {LIMPAR}")
+aluno3 = input(f"{CINZA_BOLD}Digite o nome do {VERMELHO_BOLD}4° {CINZA_BOLD}aluno: {LIMPAR}")
+print(f"{LIMPAR}{AZUL_BOLD}-{LIMPAR}"*DIV)
 
 alunos = [aluno0, aluno1, aluno2, aluno3]
 ordem_alunos = shuffle(alunos)
 
 print("ORDEM DE APRESENTAÇÃO:".center(DIV))
-print("-"*DIV)
-print(f"1° Lugar: {alunos[0]}")
-print(f"2° Lugar: {alunos[1]}")
-print(f"3° Lugar: {alunos[2]}")
-print(f"4° Lugar: {alunos[3]}")
+print(f"{AZUL_BOLD}-{LIMPAR}"*DIV)
+print(f"{VERDE_BOLD}1° {CINZA_BOLD}Lugar: {VERDE_BOLD}{alunos[0]}{LIMPAR}")
+print(f"{AZUL_BOLD}2° {CINZA_BOLD}Lugar: {AZUL_BOLD}{alunos[1]}{LIMPAR}")
+print(f"{AMARELO_BOLD}3° {CINZA_BOLD}Lugar: {AMARELO_BOLD}{alunos[2]}{LIMPAR}")
+print(f"{VERMELHO_BOLD}4° {CINZA_BOLD}Lugar: {VERMELHO_BOLD}{alunos[3]}{LIMPAR}")

34f278f3362001ea3ec25a3266d984e15573cf6d
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 15:17:31 -0300

    refactor(exercicio_#015) adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#015.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#015.py
@@ -1,23 +1,25 @@
 # Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado.
 # Calcule o preço a pagar, sabendo que o carro custa R$60,00 por dia e R$0,15 centavos por Km rodado. 
 
-print("Calculadora de Gastos (Aluguel de Carros)")
+import cores
+    
+print(f"{cores.CINZA_BOLD}Calculadora de Gastos {cores.AMARELO_BOLD}(Aluguel de Carros){cores.LIMPAR}")
 
-dias_alugados = int(input("Para começar, informe quantos dias você alugou o carro: "))
-km_percorridos = float(input("Agora, digite quantos quilômetros você percorreu: "))
+dias_alugados = int(input(f"{cores.CINZA_BOLD}Para começar, informe quantos {cores.AMARELO_BOLD}dias{cores.CINZA_BOLD} você alugou o carro: {cores.AMARELO_UNDERLINE}"))
+km_percorridos = float(input(f"{cores.LIMPAR}{cores.CINZA_BOLD}Agora, digite quantos {cores.AMARELO_BOLD}quilômetros{cores.CINZA_BOLD} você percorreu: {cores.AMARELO_UNDERLINE}"))
 
-print("Estamos calculando o seu gasto...")
-print("-" * 40)
+print(f"{cores.LIMPAR}{cores.BRANCO_BACKGROUND}Estamos calculando o seu gasto...{cores.LIMPAR}")
+print(f"{cores.AMARELO_BOLD}-{cores.LIMPAR}" * 40)
 
-preco_diaria = 60
-preco_km = 0.15
+PRECO_DIARIA = 60
+PRECO_KM = 0.15
 
-custo_aluguel = dias_alugados * preco_diaria
-custo_quilometragem = km_percorridos * preco_km
+custo_aluguel = dias_alugados * PRECO_DIARIA
+custo_quilometragem = km_percorridos * PRECO_KM
 custo_total = custo_aluguel + custo_quilometragem
 
-print(f"Você alugou o carro por {dias_alugados} dias, pelo valor de R${preco_diaria:.2f}")
-print(f"O custo do aluguel é: R${custo_aluguel:.2f}")
-print(f"O custo por quilômetros rodados é: R${custo_quilometragem:.2f}")
-print(f"Custo final: R${custo_total:.2f}")
+print(f"{cores.CINZA_BOLD}Você alugou o carro por {cores.AMARELO_BOLD}{dias_alugados}{cores.CINZA_BOLD} dias, pelo valor de {cores.VERDE_BOLD}R${cores.CINZA_BOLD}{custo_aluguel:.2f}{cores.LIMPAR}")
+print(f"{cores.CINZA_BOLD}O custo do aluguel é: {cores.VERDE_BOLD}R${cores.LIMPAR}{cores.CINZA_BOLD}{custo_aluguel:.2f}{cores.LIMPAR}")
+print(f"{cores.CINZA_BOLD}O custo por quilômetros rodados é: {cores.VERDE_BOLD}R${cores.LIMPAR}{cores.CINZA_BOLD}{custo_quilometragem:.2f}{cores.LIMPAR}")
+print(f"{cores.AMARELO_BOLD}Custo final: {cores.VERDE_BOLD}R${cores.LIMPAR}{cores.CINZA_BOLD}{custo_total:.2f}{cores.LIMPAR}")
 

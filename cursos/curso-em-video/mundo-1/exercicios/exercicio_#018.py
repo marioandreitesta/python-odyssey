0361595da4e6023d6ba524d7795136bb6e0c8e36
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 16:08:32 -0300

    refactor(exercicio_#018): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#018.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#018.py
@@ -1,24 +1,25 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo. 
 
 from math import radians, cos, sin, tan
+from cores import LIMPAR, CINZA_BOLD, ROXO_BOLD, AMARELO_BOLD, AZUL_BOLD
 
-DIV = 50
+DIV = 41
 
-print("="*DIV)
-print("CALCULADORA: SENO, COSSENO & TANGENTE".center(DIV))
-print("="*DIV)
+print(f"{CINZA_BOLD}={LIMPAR}"*DIV)
+print(f"{CINZA_BOLD}CALCULADORA: {ROXO_BOLD}SENO, {AZUL_BOLD}COSSENO & {AMARELO_BOLD}TANGENTE{LIMPAR}".center(DIV))
+print(f"{CINZA_BOLD}={LIMPAR}"*DIV)
 
-angulo = float(input("Digite o ângulo: "))
-print("-"*DIV)
+angulo = float(input(f"{CINZA_BOLD}Digite o ângulo: {LIMPAR}"))
+print(f"{CINZA_BOLD}-{LIMPAR}"*DIV)
 
-print("Calculando...".center(DIV))
-print("-"*DIV)
+print(f"{CINZA_BOLD}Calculando...".center(DIV))
+print(f"{LIMPAR}{CINZA_BOLD}-{LIMPAR}"*DIV)
 
 angulo_rad = radians(angulo)
 seno = sin(angulo_rad)
 cosseno = cos(angulo_rad)
 tangente = tan(angulo_rad)
 
-print(f"-> SENO: {seno:.2f}")
-print(f"-> COSSENO: {cosseno:.2f}")
-print(f"-> TANGENTE: {tangente:.2f}")
+print(f"{ROXO_BOLD}->{CINZA_BOLD} SENO: {ROXO_BOLD}{seno:.2f}{LIMPAR}")
+print(f"{AZUL_BOLD}->{CINZA_BOLD} COSSENO: {AZUL_BOLD}{cosseno:.2f}{LIMPAR}")
+print(f"{AMARELO_BOLD}->{CINZA_BOLD} TANGENTE: {AMARELO_BOLD}{tangente:.2f}{LIMPAR}")

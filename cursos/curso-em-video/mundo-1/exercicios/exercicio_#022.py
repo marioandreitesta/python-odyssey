8443cb97b98a7bb302a7814949e58e71706c5f63
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 18:31:24 -0300

    refactor(exercicio_#022): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#022.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#022.py
@@ -3,18 +3,20 @@
 # Quantas letras ao todo (sem considerar espaços);
 # Quantas letras tem o primeiro nome.
 
+from cores import LIMPAR, ROXO_BOLD, CINZA_BOLD
+
 DIV = 44
 
 print('' * DIV)
-print('CURIOSIDADES SOBRE O SEU NOME'.center(DIV))
+print(f'{ROXO_BOLD}CURIOSIDADES SOBRE O SEU NOME{LIMPAR}'.center(DIV))
 print('' * DIV)
 
-nome = input('Digite seu nome completo: ').strip()
-print('-' * DIV)
+nome = input(f'{CINZA_BOLD}Digite seu nome completo: {ROXO_BOLD}').strip()
+print(f'{LIMPAR}-' * DIV)
 
 nome_dividido = nome.split()
 
-print(f'Seu nome em maiúsculo: {nome.upper()}')
-print(f'Seu nome em minúsculo: {nome.lower()}')
-print(f'Quantidade de letras em seu nome completo: {len(nome)}')
-print(f'Quantidade de letras em seu primeiro nome: {len(nome_dividido[0])}')
+print(f'{CINZA_BOLD}Seu nome em maiúsculo: {ROXO_BOLD}{nome.upper()}')
+print(f'{CINZA_BOLD}Seu nome em minúsculo: {ROXO_BOLD}{nome.lower()}')
+print(f'{CINZA_BOLD}Quantidade de letras em seu nome completo: {ROXO_BOLD}{len(nome)}')
+print(f'{CINZA_BOLD}Quantidade de letras em seu primeiro nome: {ROXO_BOLD}{len(nome_dividido[0])}{LIMPAR}')

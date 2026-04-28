fbab37c958b071ac365635278b4e00c35726d48f
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 18:37:53 -0300

    refactor(exercicio_#023): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#023.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#023.py
@@ -1,20 +1,22 @@
 # Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados.
 
-DIV = 40
+from cores import LIMPAR, AMARELO_BOLD, CINZA_BOLD
+
+DIV = 30
 
 print('' * DIV)
-print('SEPARANDO DIGITOS DE UM NÚMERO'.center(DIV))
-print('=' * DIV)
+print(f'{AMARELO_BOLD}SEPARANDO DIGITOS DE UM NÚMERO{LIMPAR}'.center(DIV))
+print(f'{CINZA_BOLD}={LIMPAR}' * DIV)
 
-numero = int(input('Digite um número: '))
-print('-' * DIV)
+numero = int(input(f'{CINZA_BOLD}Digite um número: {AMARELO_BOLD}'))
+print(f'{CINZA_BOLD}-{LIMPAR}' * DIV)
 
 unidade = numero % 10
 dezena = (numero // 10) % 10
 centena = (numero // 100) % 10
 milhar = (numero // 1000) % 10
 
-print(f'Unidade: {unidade}')
-print(f'Dezena: {dezena}')
-print(f'Centena: {centena}')
-print(f'Milhar: {milhar}')
+print(f'{CINZA_BOLD}Unidade: {AMARELO_BOLD}{unidade}')
+print(f'{CINZA_BOLD}Dezena: {AMARELO_BOLD}{dezena}')
+print(f'{CINZA_BOLD}Centena: {AMARELO_BOLD}{centena}')
+print(f'{CINZA_BOLD}Milhar: {AMARELO_BOLD}{milhar}{LIMPAR}')

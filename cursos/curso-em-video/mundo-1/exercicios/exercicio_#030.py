3f3a2945b3b52cc27971380fd9a851fe3d02eaec
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-29 20:00:20 -0300

    refactor(exercicio_#030): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#030.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#030.py
@@ -1,16 +1,19 @@
 # Faça um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+from cores import AMARELO_BOLD, ROXO_BOLD, CINZA_BOLD, LIMPAR
+
 DIV = 27
 
-print('ESSE NÚMERO É PAR OU ÍMPAR?'.center(DIV))
-print('-' * DIV)
+print(f'{CINZA_BOLD}ESSE NÚMERO É {AMARELO_BOLD}PAR{CINZA_BOLD} OU {ROXO_BOLD}ÍMPAR{CINZA_BOLD}?')
+print(f'{LIMPAR}{CINZA_BOLD}-{LIMPAR}' * DIV)
 
 try:
-    numero = int(input('Digite um número: '))
+    numero = int(input(f'{CINZA_BOLD}Digite um número: {LIMPAR}'))
 
     if numero % 2 == 0:
-        print('Esse número é PAR!')
+        print(f'{AMARELO_BOLD}Esse número é PAR!{LIMPAR}')
     else:
-        print('Esse número é ÍMPAR!')
+        print(f'{ROXO_BOLD}Esse número é ÍMPAR!{LIMPAR}')
 except ValueError:
-    print('Isso não é um número, tente novamente!')
+    print(f'{CINZA_BOLD}Isso não é um número, tente novamente!{LIMPAR}')
 

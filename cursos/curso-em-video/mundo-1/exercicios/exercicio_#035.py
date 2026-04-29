7c1fdceb0c1402485fc4ba5570c9f8e9f1ee84a8
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-29 20:47:24 -0300

    refactor(exercicio_#035): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#035.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#035.py
@@ -1,13 +1,15 @@
 # Desenvolva um programa que leia três retas e diga ao usuário se elas podem ou não formar um triângulo. 
 
+import cores
+
 def receber_comprimento():
 
     while True:
-        entrada = input("\n Digite três comprimentos separados por espaço: ")
+        entrada = input(f"\n {cores.AMARELO_BOLD}Digite três comprimentos separados por espaço: {cores.LIMPAR}")
         lista = entrada.split()
 
         if len(lista) != 3:
-            print(f"Você digitou {len(lista)}, é preciso de exatamente três.")
+            print(f"{cores.CINZA_BOLD}Você digitou {cores.VERMELHO_BOLD}{len(lista)}{cores.CINZA_BOLD}, é preciso de exatamente {cores.VERDE_BOLD}três.{cores.LIMPAR}")
             continue
 
         try:
@@ -15,7 +17,7 @@ def receber_comprimento():
             return comprimentos
         
         except ValueError:
-            print("Error: Use apenas números. Ex: 20, 12, 3.7")
+            print(f"{cores.VERMELHO_BOLD}Error{cores.CINZA_BOLD}: Use apenas números. Ex: {cores.AMARELO_BOLD}20 12 3.7{cores.LIMPAR}")
 
 def calcular_triangulo(comprimentos):
 
@@ -24,9 +26,9 @@ def calcular_triangulo(comprimentos):
     c = comprimentos[2]
 
     if a + b > c and a + c > b and b + c > a: # Condição de Existência de um Triângulo
-        print("Os comprimentos informados podem formar um triângulo!")
+        print(f"{cores.CINZA_BOLD}Os comprimentos informados {cores.VERDE_BOLD}podem{cores.CINZA_BOLD} formar um triângulo!{cores.LIMPAR}")
     else:
-        print(f"Os comprimentos informados {comprimentos} não podem formar um triângulo.")
+        print(f"{cores.CINZA_BOLD}Os comprimentos informados {cores.VERMELHO_BOLD}{comprimentos} não {cores.CINZA_BOLD}podem formar um triângulo.{cores.LIMPAR}")
 
 comprimentos = receber_comprimento()
 calcular_triangulo(comprimentos)

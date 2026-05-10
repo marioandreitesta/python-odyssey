c579d0b78d533fb5ab5454e100d0db1089a52bf4
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-05-10 20:00:07 -0300

    fix(exercicio_#042): corrige lógica de comparação e adiciona output

--- mundo-2/exercicios/exercicio_#042.py
+++ mundo-2/exercicios/exercicio_#042.py
@@ -11,7 +11,7 @@ def receber_comprimentos() -> list[float]:
         entrada: str = input("Digite três comprimentos separados por espaço: ")
         lista: list[str] = entrada.split()
         limite_comprimentos: int = 3
-        if lista != limite_comprimentos:
+        if len(lista) != limite_comprimentos:
             print(f"❌ | Você digitou {len(lista)}, é preciso três números.")
         try:
             comprimentos: list[float] = list(map(float, lista))
@@ -20,11 +20,19 @@ def receber_comprimentos() -> list[float]:
         except ValueError:
             print("❌ | Use apenas números. Ex: 10, 12, 8.5")
 
+
 def processar_comprimentos(comprimentos: list[float]) -> None:
     a: float = comprimentos[0]
     b: float = comprimentos[1]
     c: float = comprimentos[2]
-    if a == b == c:
-        equilatero: list[float] = comprimentos
-    elif a == b and c != a and b:
-        print("")
+    if a + b <= c or a + c <= b or b + c <= a:
+        print("❌ | Estes comprimentos não podem formar um triângulo.")
+    elif a == b == c:
+        print(f"Estes comprimentos {comprimentos} formam um triângulo equilátero.")
+    elif a in (b, c) or b == c:
+        print(f"Estes comprimentos {comprimentos} formam um triângulo isóceles.")
+    else:
+        print(f"Estes comprimentos {comprimentos} formam um triângulo escaleno.")
+
+comprimentos: list[float] = receber_comprimentos()
+processar_comprimentos(comprimentos)
